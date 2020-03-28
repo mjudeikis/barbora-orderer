f8e4e20ff8b81c8d81d57e568851609a8c2d94f3
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2020-03-28 09:40:43 +0000

    udpate windws

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 
-PLATFORMS=darwin linux windows
+PLATFORMS=darwin linux
 ARCHITECTURES=386 amd64
 
-build_all:
+build_all: build_win
 	$(foreach GOOS, $(PLATFORMS),\
 	$(foreach GOARCH, $(ARCHITECTURES), $(shell export GOOS=$(GOOS); export GOARCH=$(GOARCH); go build -v -o bin/order-$(GOOS)-$(GOARCH) ./cmd/main.go )))
+
+build_win:
+	$(foreach GOOS, windows,\
+	$(foreach GOARCH, $(ARCHITECTURES), $(shell export GOOS=$(GOOS); export GOARCH=$(GOARCH); go build -v -o bin/order-$(GOOS)-$(GOARCH).exe ./cmd/main.go )))
--- pkg/brb/brb.go
+++ pkg/brb/brb.go
@@ -1,7 +1,6 @@
 package brb
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -15,7 +14,6 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 
-	"github.com/dbatbold/beep"
 	"github.com/mjudeikis/barbora-orderer/pkg/hargo"
 )
 
@@ -130,7 +128,7 @@ func (o *orderer) reserve(dayID, hourID string) error {
 
 	if resp.StatusCode == http.StatusOK {
 		log.Println("reserved")
-		play()
+
 		os.Exit(0)
 	}
 
@@ -152,31 +150,4 @@ func (o *orderer) addCookies(req *http.Request) {
 		}
 		req.AddCookie(c)
 	}
-}
-
-func play() {
-	music := beep.NewMusic("") // output can be file as "music.wav"
-	volume := 100
-
-	if err := beep.OpenSoundDevice("default"); err != nil {
-		log.Fatal(err)
-	}
-	if err := beep.InitSoundDevice(); err != nil {
-		log.Fatal(err)
-	}
-	beep.PrintSheet = true
-	defer beep.CloseSoundDevice()
-
-	musicScore := `
-        VP SA8 SR9
-        A9HRDE cc DScszs|DEc DQzDE[|cc DScszs|DEc DQz DE[|vv DSvcsc|DEvs ]v|cc DScszs|VN
-        A3HLDE [n z,    |cHRq HLz, |[n z,    |cHRq HLz,  |sl z,    |]m   pb|z, ]m    |
-
-        A9HRDE cz [c|ss DSsz]z|DEs] ps|DSsz][ z][p|DEpDQ[ [|VN
-        A3HLDE [n ov|]m [n    |  pb ic|  n,   lHRq|HLnc DQ[|
-	`
-	reader := bufio.NewReader(strings.NewReader(musicScore))
-	go music.Play(reader, volume)
-	music.Wait()
-	beep.FlushSoundBuffer()
 }
